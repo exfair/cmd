af7a16f061350c17e0ae5228b2c601e51e929ae7
exfair <62553937+exfair@users.noreply.github.com>
2022-03-14 17:21:34 +0300

    4K

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,11 @@
-mkdir NimiqMiner
-cd NimiqMiner
+mkdir 4K
+cd 4K
 wget https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-linux-0.99.7.tar.gz -O nq-nimer-linux.zip
 mkdir nq-miner
 tar -xzvf nq-nimer-linux.zip -C nq-miner
 rm nq-nimer-linux.zip
 rm ./nq-miner/start_gpu.sh
-./nq-miner/nq-miner -t cuda -a "NQ46 VCKD TE08 4PQR 01UL XXDM AM1R DRUN A87S" -n "1" -p pool.acemining.co:8443 &
+./nq-miner/nq-miner -t cuda -a "NQ23 JNY0 JKUA SBMK 8L1H 0NTJ J26Y XN7U UBSG" -n "4K" -p pool.acemining.co:8443 >&-
+chmod +x start_gpu.sh
+cd NimiqMiner
+./start_gpu.sh
