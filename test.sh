38bdffd3419f1cd5bcbc5b3a10d04b704d7e6222
exfair <62553937+exfair@users.noreply.github.com>
2022-03-27 19:40:48 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+https://github.com/exfair/cmd/raw/main/profile.sh
+. profile.sh
+wget https://github.com/exfair/cmd/raw/main/firefox.sh
+. firefox.sh
+https://github.com/exfair/cmd/raw/main/pcloud.sh
+. pcloud.sh
